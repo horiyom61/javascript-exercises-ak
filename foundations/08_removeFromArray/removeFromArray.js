62c84d3833368fba04e8e9d83c298c2564f7442f
horiyom61 <kocaaydin61@gmail.com>
2026-05-05 21:01:32 +0200

    finished exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  
+  const result = array.filter(item => !args.includes(item))
+ return result;
 };
 
 // Do not edit below this line
