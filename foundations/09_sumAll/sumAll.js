d3e9216378dd7570abea465d74e6d2b0b08e6ad7
horiyom61 <kocaaydin61@gmail.com>
2026-05-05 21:32:03 +0200

    finished exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+  let resultaat = 0
+ 
+  let small = Math.min(min, max)
+  let big = Math.max(min, max)
 
+    if(small < 0) {
+    return "ERROR"
+  } 
+
+ if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR"
+ }
+ 
+  for(let i = small; i <= big; i++) {
+     resultaat = resultaat + i;
+  }
+  return resultaat;
+  
 };
 
 // Do not edit below this line
