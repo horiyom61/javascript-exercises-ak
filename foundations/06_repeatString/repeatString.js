7dfbb36dd7498ecd276b16c6916b93df5ee64586
horiyom61 <kocaaydin61@gmail.com>
2026-05-05 20:32:47 +0200

    finished exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let resultaat = "";
+for(let i = 0; i < num; i++){
+  resultaat = resultaat + string;
+}
+  if(num < 0){
+    return "ERROR"
+  }
+  return resultaat
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
