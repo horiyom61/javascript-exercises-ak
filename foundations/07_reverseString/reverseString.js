68d2daee6356f0ac1b33cddd75ec733dd409cf1d
horiyom61 <kocaaydin61@gmail.com>
2026-05-05 20:44:07 +0200

    finished exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+ let result = ""
+  let part = str.split("")
+  part.reverse()
+  let get = part.join("")
+  console.log(get)
+  return result + get
 };
 
 // Do not edit below this line
